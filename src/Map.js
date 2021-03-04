3c5dd3dd2c3d7263b1d827cd2f65c72c02f8a8fd
seungmin_dev <seungmin.dev@gmail.com>
2021-03-04 21:59:33 +0900

    Add circle on map

--- src/Map.js
+++ src/Map.js
@@ -3,69 +3,53 @@ const {kakao} = window;
 
 const MapCom = () => {
     const [mapping, isMapping] = useState(true);
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(35.157588, 129.058822), //지도의 중심좌표.
-        level: 4 //지도의 레벨(확대, 축소 정도)
-    };
     const getContainer = async () => {
         const container = await document.getElementById("map");
-        console.log('container:',container);const map = setTimeout(() => { //지도 생성 및 객체 리턴
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var lat = position.coords.latitude, // 위도
+                    lon = position.coords.longitude; // 경도
+                    console.log(lat, lon);
+                setTimeout(createMap, 1000, lat, lon, container);
+            });
+        } else {
+            var lat = '37.57598282289137';
+            var lon = '126.97680910426234';
+            setTimeout(createMap, 1000, lat, lon, container);
+        }
+    };
+
+    const createMap = (lat, lon, container) => {
+        const options = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(lat, lon), //지도의 중심좌표.
+            level: 4 //지도의 레벨(확대, 축소 정도)
+        };
+        console.log('createOptions:',options);
+        const map = () => { //지도 생성 및 객체 리턴
             new kakao.maps.Map(container, options);
-        }, 1000);
+        };
+        // const center = {
+        //     lat : lat, lon : lon
+        // };
+        // const circle = () => {
+        //     new kakao.maps.Circle({
+        //         center : center,  // 원의 중심좌표 입니다 
+        //         radius: 1000, // 미터 단위의 원의 반지름입니다 
+        //         strokeWeight: 5, // 선의 두께입니다 
+        //         strokeColor: '#75B8FA', // 선의 색깔입니다
+        //         strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        //         strokeStyle: 'dashed', // 선의 스타일 입니다
+        //         fillColor: '#CFE7FF', // 채우기 색깔입니다
+        //         fillOpacity: 0.7  // 채우기 불투명도 입니다   
+        //     });
+        // };
+        // setTimeout(circle.setMap(map), 1000); 
     };
 
     useEffect(() => {
         getContainer();
+        //map.setCenter(locPosition); 
     }, []);
-    
-    // if (navigator.geolocation) {
-    
-    //     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    //     navigator.geolocation.getCurrentPosition(function(position) {
-            
-    //         var lat = position.coords.latitude, // 위도
-    //             lon = position.coords.longitude; // 경도
-            
-    //         var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-    //             message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-            
-    //         // 마커와 인포윈도우를 표시합니다
-    //         displayMarker(locPosition, message);
-                
-    //       });
-        
-    // } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-        
-    //     var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
-    //         message = 'geolocation을 사용할수 없어요..'
-            
-    //     displayMarker(locPosition, message);
-    // }
-    
-    // // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    // function displayMarker(locPosition, message) {
-    
-    //     // 마커를 생성합니다
-    //     var marker = new kakao.maps.Marker({  
-    //         map: map, 
-    //         position: locPosition
-    //     }); 
-        
-    //     var iwContent = message, // 인포윈도우에 표시할 내용
-    //         iwRemoveable = true;
-    
-    //     // 인포윈도우를 생성합니다
-    //     var infowindow = new kakao.maps.InfoWindow({
-    //         content : iwContent,
-    //         removable : iwRemoveable
-    //     });
-        
-    //     // 인포윈도우를 마커위에 표시합니다 
-    //     infowindow.open(map, marker);
-        
-    //     // 지도 중심좌표를 접속위치로 변경합니다
-    //     map.setCenter(locPosition);      
-    // }  
 
     return(
         <div className="popup">
