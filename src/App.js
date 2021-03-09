743aa16f0d16875cfffc13dfc8068d4168d280bc
seungmin_dev <seungmin.dev@gmail.com>
2021-03-10 06:53:21 +0900

    TODO : How to set coords

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import { render } from "@testing-library/react";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import MapCom from "./Map";
 import Weather from "./Weather";
 
 function App() {
-  const [coords, setCoords] = useState(null);
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setCoords({
-        lat : position.coords.latitude,
-        lon : position.coords.longitude
-      });
-    });
-  } else {
-    alert("지도 위치 확인을 허용해주세요!");
+  const [init, setInit] = useState(false);
+  const [coords, setCoords] = useState("");
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 3000,
+    maximumAge: 0
+  };
+  function success(pos) {
+    console.log('pos.coords.latitude:',pos.coords.latitude);
+
+    // setCoords({
+    //   lat : pos.coords.latitude,
+    //   lon : pos.coords.longitude
+    // });
+    setCoords(pos.coords.latitude, pos.coords.longitude);
+    console.log('coords:',coords);
   }
+  function error(err) {
+    console.log('error:',err);
+  }
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+      alert("지도 위치 확인을 허용해주세요!");
+      setCoords(null);
+    }
+    setInit(true);
+  }, []);
   return (
     <div className="App">
       <MapCom />
-      <Weather coords={coords} />
+      {/* {init ? <Weather lat={coords.lat} lon={coords.lon} /> : "Initializing..."} */}
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,15 +2,14 @@ import { render } from "@testing-library/react";
 import React, {useState} from "react";
 import {weatherApi} from "./api";
 
-const Weather = async ({coords}) => {
-    console.log('coords:',coords);
+const Weather = async (lat, lon) => {
     // 위도 경도 apiKey 보내기
     const [weather, setWeather] = useState(false);
-    const [temps, setTemps] = useState(false);
+    // const [temps, setTemps] = useState(false);
     setWeather(true);
-    const {main : {temp}} = await weatherApi(coords.lat, coords.lon);
-    console.log('temp:', temp);
-    setTemps(true);
+    // const {main : {temp}} = await weatherApi(coords.lat, coords.lon);
+    // console.log('temp:', temp);
+    // setTemps(true);
 
     return (
         <>
