4346b180bda46af0b9e31dd36246b82b056e6fd2
seungmin_dev <seungmin.dev@gmail.com>
2021-03-15 06:41:44 +0900

    Add things

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Board = () => {
+    const [write, setWrite] = useState(false);
+    
+    // 익명의 ip로 글 쓴 사람 판별(ip 컴포넌트에서 ip값 넘겨오기)
+    // 로그인 없이 일단 글 쓸 수 있고
+    // 해당 글의 아이피와 접속 아이피가 같으면 수정, 삭제 가능
+    // 아니라면 불가능
+    // 글 쓰고 아이피, 저장시각, 내용 -> 파이어스토어
 
     return (
-        <span>Board!</span>
+        <div className="board">
+            <span className="text"></span>
+        </div>
     )
 }
 
--- src/Ip.js
+++ src/Ip.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {userIpApi} from "./api";
 
 const Ip = () => {
@@ -9,17 +9,26 @@ const Ip = () => {
    // var data = JSON.stringify(data, null, 2);
     const [init, setInit] = useState(false);
     const [userIp, setUserIp] = useState("");
-    fetch(
-        `https://api.ipdata.co/?api-key=1ab80a79ea5f35a9d21e9b64415a3e1a08f30b0118f7f44223247e24`
-    ).then(function(response) {
-        return response.json()
-    }).then(function(json) {
+    const getApi = async () => {
+        const {data} = await userIpApi();
+        console.log('data:',data);
         setInit(true);
-        setUserIp({ip:json.ip});
-    });
+        // setUserIp({ip});
+    }
+    useEffect(() => {
+        getApi();
+    }, []);
+    // fetch(
+    //     ``
+    // ).then(function(response) {
+    //     return response.json()
+    // }).then(function(json) {
+    //     setInit(true);
+    //     setUserIp({ip:json.ip});
+    // });
     return (
         <>
-            {userIp ? <span>{setUserIp.ip}</span> : "Can't get user's Ip" }
+            {/* {userIp ? <span>{setUserIp.ip}</span> : "Can't get user's Ip" } */}
         </>
     )
 }
