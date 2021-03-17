894fc74dca540b2238ab40f282b8dee65b422b0d
seungmin_dev <seungmin.dev@gmail.com>
2021-03-18 06:59:47 +0900

    TODO - Edit, Delete, Report

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react";
+import { dbService, storageService } from "./fbase";
 
 const Content = ({key, contentArr, isOwner}) => {
-    console.log('key:',key);
-    console.log('contentArr:',contentArr);
-    console.log('isOwner:',isOwner);
+    const [editting, setEditting] = useState(false);
+    const [newContent, setNewContent] = useState(contentArr.content);
     let d = new Date();
     let day = d.getDay();
     let dayKor = "";
@@ -12,14 +12,46 @@ const Content = ({key, contentArr, isOwner}) => {
     } else {
         dayKor = "오늘 "
     }
+
+    const onChange = (event) => {
+        event.preventDefault();
+        const {target:{value}} = event;
+        setNewContent(value);
+    }
+    const toggleEdit = () => setEditting((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        console.log({...contentArr});
+        await dbService.doc(`ㅁㅇㅇㅇ/${contentArr.id}`).update({
+            content : newContent
+        });
+        setEditting(false);
+    }
+    const DeleteContent = () => {
+
+    }
+    const ReportContent = () => {
+        
+    }
     return (
         <>
-            <div className="contentBox">
-                <span>랜선친구 {contentArr.randomId}</span>
-                {isOwner ? <><button>수정</button><button>삭제</button></> : <button>신고</button>}<br/>
-                <span>{dayKor}{contentArr.time}</span><br/>
-                <span>{contentArr.content}</span>
-            </div>
+            {editting ? (
+                    <>
+                    <form className="contentBox" onSubmit={onSubmit}>
+                        <input className="" value={newContent} onChange={onChange} name="newContent" placeholder="내용을 수정해주세요"></input>
+                        <input type="submit" value="수정완료" className="formBtn" />
+                    </form>
+                    <button onClick={toggleEdit}>수정취소</button>
+                    </>
+                )
+            :
+                <div className="contentBox">
+                    <span>랜선친구 {contentArr.randomId}</span>
+                    {isOwner ? <><button onClick={toggleEdit}>수정</button><button onClick={DeleteContent}>삭제</button></> : <button onClick={ReportContent}>신고</button>}<br/>
+                    <span>{dayKor}{contentArr.time}</span><br/>
+                    <span>{contentArr.content}</span>
+                </div>
+            }
         </>
     )
 }
